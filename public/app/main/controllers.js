061d34945f25308789db23bc21ea6543a4c49e36
Doua Thao <douathoj@gmail.com>
2014-11-01 12:42:49 -0500

    remove debug

--- public/app/main/controllers.js
+++ public/app/main/controllers.js
@@ -4,14 +4,6 @@ angular.module('cash4MyDevice.main')
     '$stateParams',
     'devices',
     function ($scope, $stateParams, devices) {
-      /********************************************************/
-      console.group('%cdevices :', 'color:red;font:strait');
-      console.log(devices);
-      console.groupEnd();
-      /********************************************************/
       $scope.devices = devices.getDevices($stateParams.type);
-      console.log('*****************  devices  **************************');
-      console.log(devices);
-      console.log('**********************************************');
       $scope.type = $stateParams.type;
     }]);
